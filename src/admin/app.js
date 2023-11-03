d5ebbdc1c6b214627a9f16a435aa771759685dad
jtalamantez <joe@cabdash.com>
2023-11-03 00:19:41 -0500

    customize to SRL

---
+++ src/admin/app.js
@@ -0,0 +1,66 @@
+import MenuLogo from './extensions/srl.png'
+import AuthLogo from './extensions/srl.png';
+
+export default {
+    config: {
+        theme:{
+            colors:{
+                primary100:"#ebc7b4",
+                primary200:"#cea490",
+                primary300:"#b0836b",
+                primary400:"#9a694f",
+                primary500:"#845034",
+                primary600:'#78462e',
+                primary700:'#683A25',
+                buttonPrimary500: '#845034',
+                buttonPrimary600: '#78462e',
+            }
+        },
+        auth: {
+            logo: AuthLogo,
+        },
+        menu: {
+            logo: MenuLogo
+        },
+      locales: [
+        // 'ar',
+        // 'fr',
+        // 'cs',
+        // 'de',
+        // 'dk',
+        // 'es',
+        // 'he',
+        // 'id',
+        // 'it',
+        // 'ja',
+        // 'ko',
+        // 'ms',
+        // 'nl',
+        // 'no',
+        // 'pl',
+        // 'pt-BR',
+        // 'pt',
+        // 'ru',
+        // 'sk',
+        // 'sv',
+        // 'th',
+        // 'tr',
+        // 'uk',
+        // 'vi',
+        // 'zh-Hans',
+        // 'zh',
+      ],
+      translations: {
+        en: {
+          'app.components.LeftMenu.navbrand.title': 'Snake River Lodge',
+          "app.components.LeftMenu.navbrand.workplace": "Backend",
+          "Auth.form.welcome.subtitle": "Log in to your account",
+          "Auth.form.welcome.title": "",
+        }
+      },
+    },
+    
+    bootstrap() {},
+};
+
+
