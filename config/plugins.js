5c4e821c72807b3f49ea90aee5f3f4f42face08f
jtalamantez <joe@cabdash.com>
2023-11-17 09:41:08 -0600

    diet to recipe, drips and drops

--- config/plugins.js
+++ config/plugins.js
@@ -6,5 +6,8 @@ module.exports = ({ env }) => ({
         // See `Config` section.
       },
     },
+    'filter-button': {
+        enabled: true,
+    },
     //...
   });
--- src/admin/webpack.config.js
+++ src/admin/webpack.config.js
@@ -2,8 +2,36 @@
 
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
-module.exports = (config) => {
-  config.plugins.push(new MonacoWebpackPlugin());
-
-  return config;
+module.exports = (config, webpack) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        CONTENT_TYPES: JSON.stringify(
+          [
+            {
+              from: 'api::topic.topic', // collection where button will be added
+              to: 'api::card.card', // which collection you want to filter
+              filterBy: 'title', // field name by which you want to filter
+              attributeName: 'viewCards', //attribute name added while adding button
+              buttonTitle: 'View Cards', // title of button
+            },
+            {
+              from: 'api::category.category',
+              to: 'api::topic.topic',
+              filterBy: 'title',
+              attributeName: 'viewTopics',
+              buttonTitle: 'View Topics',
+            },
+            {
+              from: 'api::category.category', //adding multiple buttons for same collection
+              to: 'api::card.card',
+              filterBy: 'title',
+              attributeName: 'viewCards',
+              buttonTitle: 'View Cards',
+            }
+          ]
+        ),
+      })
+    )
+    config.plugins.push(new MonacoWebpackPlugin());
+    return config;
 };
