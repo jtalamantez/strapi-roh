a33c1102ced069a42de24d830dc161b83d09692e
jtalamantez <joe@cabdash.com>
2024-01-11 23:09:51 -0600

    Added s3 bucket

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,34 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+      /* Replace 'strapi::security', with this snippet */
+      /* Beginning of snippet */
+      {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                'strapi-images-roh.s3.us-east-2.amazonaws.com',
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                'strapi-images-roh.s3.us-east-2.amazonaws.com',
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      },
+      /* End of snippet */
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
