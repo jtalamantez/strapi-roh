d7e850687212a1c83ddd6d11d24e1ec24bd96431
jtalamantez <joe@cabdash.com>
2024-02-01 21:50:27 -0600

    hardocde env variables

--- config/plugins.js
+++ config/plugins.js
@@ -16,13 +16,13 @@ module.exports = ({ env }) => ({
         config: {
           provider: 'aws-s3',
           providerOptions: {
-            accessKeyId: env('AWS_ACCESS_KEY_ID'),
-            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            accessKeyId: env('AAKIA47CR3GF3236BWWDN'),
+            secretAccessKey: env('DwTRsJK39NQ0h2GHUWRBvyzQsiJrQhqYQkCeU6f0'),
             region: 'us-east-1',
             params: {
               ACL: env('AWS_ACL', 'public-read'),
               signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-              Bucket: env('AWS_BUCKET'),
+              Bucket: env('strapi-latest-131'),
             },
           },
           actionOptions: {
